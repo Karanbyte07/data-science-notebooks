1a04e1a4b967879a6103d369f42986af17dcafa1
karanjeet kumar <karanbyte07@gmail.com>
2026-06-30 17:07:54 +0530

    flash messaging, and dynamic query parameters

--- Web Development/template inherintance/main.py
+++ Web Development/template inherintance/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 
 app = Flask(__name__)
+app.secret_key = '2580'
 
 @app.route('/')
 def home():
@@ -8,7 +9,15 @@ def home():
 
 @app.route('/about')
 def about():
+    flash("Flash message, this is the first message")
+    flash("Flash message, this is the second message")
+    flash("Flash message, this is the third message")
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    name = request.args.get('name')
+    language = request.args.get('language')
+    return render_template('contact.html', name = name, language = language)
 if __name__ == '__main__':
     app.run(debug=True)
